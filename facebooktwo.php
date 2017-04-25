3aecbfeb68536b62e20aa7a73dd75714d75e78ca
Mortenwold <mmoorrtteenn94@hotmail.com>
2017-04-25 13:15:57 +0200

    hmm

--- facebooktwo.php
+++ facebooktwo.php
@@ -24,23 +24,19 @@
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand" href="#">Fixed navbar</a>
+      <img id="unikiaicon" src="images/unikia-link.png">
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="index.html">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="analyticsdashboard.html">Google Analytics</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
+            <a class="nav-link" href="facebooktwo.php">Facebook</a>
           </li>
         </ul>
-        <form class="form-inline mt-2 mt-md-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
       </div>
     </nav>
       <div id ="facebook">
@@ -69,7 +65,7 @@ and open the template in the editor.
         $helper = $fb->getRedirectLoginHelper();
         
         // app directory could be anything but website URL must match the URL given in the developers.facebook.com/apps
-        define('APP_URL', 'http://localhost/unikia_app/index.html');
+        define('APP_URL', 'http://student.cs.hioa.no/~s236326/Betaanalytics/facebooktwo.php');
         $permissions = ['user_posts', 'user_photos']; // optional
 
         try {
