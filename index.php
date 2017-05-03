5e48efc91070db4b3fd538898febc09810d7aad5
Aksel <aksel.christoffer.wiig@gmail.com>
2017-05-03 14:28:35 +0200

    !

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         <title>Unikia Dashboard</title>
     </head>
 
-    <body id="mainbody">
+    <body>
         <nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
