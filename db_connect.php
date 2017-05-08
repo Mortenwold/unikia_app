e1ddb46acac61de34d83119686672da83b849bbd
Aksel <aksel.christoffer.wiig@gmail.com>
2017-05-08 16:37:40 +0200

    server DB

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = mysqli_connect("localhost", "root", "", "unikia");
+$db = mysqli_connect("student.cs.hioa.no", "s232324", "", "s232324");
 if (!$db) {
     trigger_error(mysqli_error($db));
     die("Kunne ikke knytte til server");
